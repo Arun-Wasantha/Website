1ff20cda5d2ad7852d188f34605a0932e427f70d
H.P.A.W Kumara <tech@wediditglobal.com>
2021-11-10 18:50:07 +0530

    intitial deployment Commit

---
+++ javascript/report_form.js
@@ -0,0 +1,57 @@
+console.log("Report Form Loaded")
+
+
+$("#reportForm-submit-button").click(
+    () => {
+        $('#reportForm-submit-button').val("Sending Request please wait...")
+
+        let email = $("#inputfield-email").val();
+        let contentName = $("#inputfield-contentName").val();
+        let mediaType = $("#inputfield-mediaType").val();
+        let details = $("#inputfield-details").val();
+        let url = $("#inputfield-url").val();
+
+        console.log(email, contentName, mediaType, details, url)
+
+        let payload = {
+            email : email,
+            contentName : contentName,
+            mediaType : mediaType,
+            additionalDetails : details,
+            url : url
+        }
+
+        fetch(
+            "https://us-central1-wediditwebsite.cloudfunctions.net/addCopyrightInfringementReport",
+            {
+              method: "post",
+              headers: {
+                "Content-Type": "application/json",
+               // "Access-Control-Allow-Origin" : "no-cors" // ONLY FOR TESTING REMOVE IN PRODUCTION
+              },
+              body: JSON.stringify(payload),
+            }
+          )
+            .then((res) => {
+                console.log(res)
+                  if(res.status === 200){
+                      console.log("Request Successfull")
+                      $('#reportForm-submit-button').css('background-color', '#007A52')
+                      $('#reportForm-submit-button').val('Request Sent ')
+                      
+                      $('.form').css('display', 'none')
+                      $('.title').text("Request Has Been Sent , We will contact you shortly")
+      
+                  }else{
+                      console.log("Request Failed")
+                      $('#reportForm-submit-button').val("Couldnt Send Your Request Please contact (+94) 113 619 765 ")
+                  }
+      
+              })
+            .catch((err) => {
+              console.log(err);
+              console.log("Request Failed")
+              $('#reportForm-submit-button').val("Couldnt Send Your Request Please contact (+94) 113 619 765 ")
+            });
+    }
+)
